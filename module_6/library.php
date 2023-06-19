072e887f8c06771a90f39f0e06aa675b6e564fd1
vladSHOKO <kuzia_vladik@mail.ru>
2023-06-19 16:22:03 +0300

    corrected code style

--- module_6/library.php
+++ module_6/library.php
@@ -3,53 +3,52 @@
 $library = [
     'authors' => [
         'john_makkormik@example.com' => [
-            'name' => 'Джон Маккормик',
-            'email' => 'john_makkormik@example.com',
-            'birthYear' => 1972,
-        ],
+            'name' => 'Джон Маккормик' ,
+            'email' => 'john_makkormik@example.com' ,
+            'birthYear' => 1972 ,
+        ] ,
         'martin_robert@example.com' => [
-            'name' => 'Мартин Роберт',
-            'email' => 'martin_robert@example.com',
-            'birthYear' => 1952,
-        ],
+            'name' => 'Мартин Роберт' ,
+            'email' => 'martin_robert@example.com' ,
+            'birthYear' => 1952 ,
+        ] ,
         'martin_fauler@example.com' => [
-            'name' => 'Мартин Фаулер',
-            'email' => 'martin_fauler@example.com',
-            'birthYear' => 1963,
-        ],
-    ],
+            'name' => 'Мартин Фаулер' ,
+            'email' => 'martin_fauler@example.com' ,
+            'birthYear' => 1963 ,
+        ] ,
+    ] ,
     'books' => [
         [
-            'title' => 'Чистый код: создание, анализ и рефакторинг',
-            'author' => 'martin_robert@example.com',
-            'year' => 2013,
-        ],
+            'title' => 'Чистый код: создание, анализ и рефакторинг' ,
+            'author' => 'martin_robert@example.com' ,
+            'year' => 2013 ,
+        ] ,
         [
-            'title' => 'Девять алгоритмов, которые изменили будущее',
-            'author' => 'john_makkormik@example.com',
-            'year' => 2011,
-        ],
+            'title' => 'Девять алгоритмов, которые изменили будущее' ,
+            'author' => 'john_makkormik@example.com' ,
+            'year' => 2011 ,
+        ] ,
         [
-            'title' => 'Идеальный программист',
-            'author' => 'martin_robert@example.com',
-            'year' => 2011,
-        ],
+            'title' => 'Идеальный программист' ,
+            'author' => 'martin_robert@example.com' ,
+            'year' => 2011 ,
+        ] ,
         [
-            'title' => 'Шаблоны корпоративных приложений',
-            'author' => 'martin_fauler@example.com',
-            'year' => 2002,
-        ],
-    ],
+            'title' => 'Шаблоны корпоративных приложений' ,
+            'author' => 'martin_fauler@example.com' ,
+            'year' => 2002 ,
+        ] ,
+    ] ,
 ];
 
 $title = 'Библиотека программиста';
-$countOfAuthors = count($library['authors']);
-$red = (bool) rand(0, 1);
+$countOfAuthors = count ( $library[ 'authors' ] );
+$red = (bool) rand ( 0 , 1 );
 
-if ($red) {
+if ( $red ) {
     $red = "red";
-}
-else {
+} else {
     $red = " ";
 }
 ?>
@@ -61,11 +60,12 @@ else {
     <link rel="stylesheet" , href="HomeworkStyle.css" , type="text/css"
 </head>
 <body class="main">
-<h1 class="<?=$red?>"><?= $title ?></h1>
+<h1 class="<?= $red ?>"><?= $title ?></h1>
 <div>Авторов на портале: <?= $countOfAuthors ?></div>
-<?php foreach ($library['books'] as $book) {?>
+<?php foreach ($library[ 'books' ] as $book) { ?>
     <p class="grey">
-        Книга <b><?= $book['title']?></b>, ее написал <?= $library['authors'][$book['author']]['name']?>, <?= $library['authors'][$book['author']]['birthYear']?> (<a href="#"><?= $book['author']?></a>)
+        Книга <b><?= $book[ 'title' ] ?></b>, ее написал <?= $library[ 'authors' ][ $book[ 'author' ] ][ 'name' ] ?>
+        , <?= $library[ 'authors' ][ $book[ 'author' ] ][ 'birthYear' ] ?> (<a href="#"><?= $book[ 'author' ] ?></a>)
     </p>
 <?php } ?>
 </body>
