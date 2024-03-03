8e918807f37461a147d35abe9fb407019d5b8135
vladSHOKO <vladislav_kuzminov.00@mail.ru>
2024-03-03 11:08:46 +0300

    Fix mistakes

--- module_20/data/authorization.php
+++ module_20/data/authorization.php
@@ -3,6 +3,6 @@
 namespace Module\Twenty;
 
 if (isset($_POST['login']) && isset($_POST['password'])) {
-    $user = new Authorization(Db::getConnection());
+    $user = new UserRepository(Db::getConnection());
     $user->validateUser();
 }
